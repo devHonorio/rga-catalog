c84570a9e353e618d4130545fed5c57e43d95a72
José Honorio de Oliveira Sobrinho Neto <honorio.dev@gmail.com>
2025-01-29 15:21:42 -0300

    feat: create component `<Header/>`

---
+++ src/components/header/header.tsx
@@ -0,0 +1,8 @@
+import { Logo } from "../logo";
+
+export const Header = () => (
+  <header className="flex items-center justify-between py-2">
+    <Logo />
+    <span>RGA Doces e Salgados</span>
+  </header>
+);
