bd432501814eafce5ed04fa7e66fbb6806a04138
rgadocesesalgados <167325494+rgadocesesalgados@users.noreply.github.com>
2025-02-25 13:12:41 +0000

    chore: update data

--- src/app/catalog/[category]/page.tsx
+++ src/app/catalog/[category]/page.tsx
@@ -1,14 +1,7 @@
 import { ProductCard } from "@/components/product-card";
-import { categorysName, dataCatalog, recheios } from "@/data";
+import { categorysName, dataCatalog } from "@/data";
 import { CategoryType } from "@/types";
 import { defaultParams, toBRL } from "@/utils";
-import {
-  Card,
-  CardDescription,
-  CardImage,
-  CardTitle,
-  CradTextContrast,
-} from "@/components/card";
 
 interface CategoryProps {
   params: Promise<{ category: string }>;
@@ -19,30 +12,12 @@ export default async function Category({ params }: CategoryProps) {
 
   const category = defaultParams({
     param: paramCategory,
-    data: ["Recheios", ...categorysName],
+    data: categorysName,
   }) as CategoryType;
 
-  const categoryRecheiosTrue = category === "Recheios";
-  const products = !categoryRecheiosTrue ? dataCatalog[category] : [];
+  const products = dataCatalog[category];
   return (
     <div className="flex flex-wrap justify-center gap-14">
-      {categoryRecheiosTrue &&
-        recheios.map(({ id, image, name, price }) => (
-          <Card key={id}>
-            <CardImage image={image} title={name} />
-            <CardTitle>{name}</CardTitle>
-
-            <CardDescription>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Perspiciatis tenetur odio eligendi numquam praesentium culpa vel
-              alias fuga cupiditate! Quibusdam ipsam porro debitis sapiente a
-              dolorem impedit beatae aliquam mollitia?
-            </CardDescription>
-
-            <CradTextContrast>{toBRL(price)}/kg</CradTextContrast>
-          </Card>
-        ))}
-
       {products.map(({ id, price, image, title }) => {
         return (
           <ProductCard key={id} title={title} image={image} price={price} />
